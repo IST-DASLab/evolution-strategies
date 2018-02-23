4b720f3bac6a17957e1652b763f1f46f642df920
Martin Hafskjold Thoresen <martinhath@gmail.com>
2018-02-23 14:26:22 +0100

    Rewrite to use a Class instead, for simpler method overriding

--- base.py
+++ base.py
@@ -6,80 +6,78 @@ import torch
 import numpy as np
 import torch.multiprocessing as mp
 
-TIME = False
 
-RANK = 0
+class ES():
+    def __init__(self, model):
+        self.model = model
+        self.seed = int(random.random() * (2 ** 32))
+        self.debug = False
 
-def id(*args, **kwargs):
-    return args[0]
+    def perturb(self, model, seed):
+        raise NotImplementedError('Please supply an `perturb` method when subclassing ES')
+
+    def evaluate(self, model):
+        raise NotImplementedError('Please supply an `evaluate` method when subclassing ES')
+
+    def pre_perturb(self, model):
+        return model
+
+    def post_perturb(self, model):
+        return model
+
+    def aggregate_results(results):
+        '''Take a list of results from `evaluate()`, and transform it somehow.
+        The output of this function is later passed to `reconstruct`.'''
+        pass
+
+    def reconstruct(self, model, aggr):
+        raise NotImplementedError('Please supply a `reconstruct` method when subclassing ES')
 
 
-def thread_loop(in_queue,
-                out_queue,
-                model,
-                args):
+def thread_loop(
+        es,
+        in_queue,
+        out_queue,
+        start_barrier,
+        end_barrier,
+        init_seed,
+        num_epochs=100,
+        offspring_per_process=100):
     '''
     This is what each thread performs. It works something like this:
 
     >>> while True:
     >>>     model = noise_model(model)
-    >>>     model_q = quantize(model)
     >>>     score = evaluate(model_q)
     >>>     broadcast(score)
     >>>     k_best = get_k_best()
     >>>     model = avg(k_best)
 
     '''
-    random.seed(args['seed'])
-    global RANK
-    RANK = args['rank']
+    random.seed(init_seed)
 
-    quantize = args.get('quantize', id)
-    noise_model = args['noise_model']
-    evaluate = args['evaluate']
-    get_next_model_child = args['get_next_model_child']
-    perturbs_per_process = args['perturbs_per_process']
-
-    for _ in range(args['num_epochs']):
-        # t0 = time()
-        args['start_barrier'].wait()
-        # t1 = time()
-        # t2 = 0.0
-        # t3 = 0.0
-        # t4 = 0.0
-        # t5 = 0.0
-        for _ in range(perturbs_per_process):
+    for _ in range(num_epochs):
+        start_barrier.wait()
+        for _ in range(offspring_per_process):
             s = int(random.random() * (2**32))
-            # _t0 = time()
-            model_n = noise_model(copy.deepcopy(model), s)
-            # _t1 = time()
-            model_q = quantize(model_n)
-            # _t2 = time()
-            score = evaluate(model_q)
-            # _t3 = time()
+            model_ = copy.deepcopy(es.model)
+            pre = es.pre_perturb(model_)
+            perturbed = es.perturb(pre, s)
+            post = es.post_perturb(perturbed)
+            score = es.evaluate(post)
             out_queue.put((score, s))
-            # _t4 = time()
-            # t2 += _t1 - _t0
-            # t3 += _t2 - _t1
-            # t4 += _t3 - _t2
-            # t5 += _t4 - _t3
-        # t6 = time()
-        model = in_queue.get()
-        # t7 = time()
-        args['end_barrier'].wait()
-        # t8 = time()
-        # if RANK == 1:
-        #     print('start wait   {:2.6f}'.format(t1 - t0))
-        #     print('noise model  {:2.6f}'.format(t2))
-        #     print('quantize     {:2.6f}'.format(t3))
-        #     print('evaluate     {:2.6f}'.format(t4))
-        #     print('out Q put    {:2.6f}'.format(t5))
-        #     print('in Q get     {:2.6f}'.format(t7 - t6))
-        #     print('end wait     {:2.6f}'.format(t8 - t7))
+        aggr = in_queue.get()
+        es.model = es.reconstruct(es.model, aggr)
+        end_barrier.wait()
 
 
-
-def main_loop(args, debug=False, seed=None):
+def main_loop(
+        es,
+        num_epochs=100,
+        offspring_per_process=100,
+        num_processes=1,
+        seed=None,
+        save_model=None):
     '''
     The main loop of the program. This is where we spawn processes, and do everything.
     On our side, the main loop looks like this:
@@ -92,7 +90,6 @@ def main_loop(args, debug=False, seed=None):
     Somehow we must also shut down the threads?
     '''
 
-    TIME = debug
     if seed:
         random.seed(seed)
         np.random.seed(seed)
@@ -100,55 +97,42 @@ def main_loop(args, debug=False, seed=None):
 
     torch.set_num_threads(1)
 
-    num_epochs = args['num_epochs']
-    num_processes = args['num_processes']
-    perturbs_per_process = args['perturbs_per_process']
-
-    args['start_barrier'] = mp.Barrier(num_processes)
-    args['end_barrier'] = mp.Barrier(num_processes)
+    start_barrier = mp.Barrier(num_processes)
+    end_barrier = mp.Barrier(num_processes)
 
     model_queue = mp.Queue()
     score_queue = mp.Queue()
-    model = args['get_model']()
-
-    get_next_model_main = args['get_next_model_main']
-    get_next_model_child = args['get_next_model_child']
-    save_model = args.get('save_model', None)
 
     # Start all processes
     processes = []
     for rank in range(num_processes):
-        args['seed'] = int(random.random() * (2**32))
-        args['rank'] = rank
-        p = mp.Process(target=thread_loop, args=(model_queue, score_queue, model, args))
+        es.debug = rank == 0
+        p = mp.Process(target=thread_loop, args=(
+            es,
+            model_queue,
+            score_queue,
+            start_barrier,
+            end_barrier,
+            int(random.random() * (2**32))),
+            kwargs={
+                'num_epochs': num_epochs,
+                'offspring_per_process': offspring_per_process,
+            })
         p.start()
         processes.append(p)
 
     start_time = time()
     for epoch in range(num_epochs):
-        t0 = time()
-        results = [score_queue.get() for _ in range(num_processes * perturbs_per_process)]
-        t1 = time()
-        results.sort() # sort by accuracy
-        t2 = time()
-        # print(results[:5])
-        model_builder = get_next_model_main([(score, seed) for ((score, _), seed) in results])
-        t3 = time()
-        model = get_next_model_child(model, model_builder)
-        t4 = time()
+        results = [score_queue.get() for _ in range(num_processes * offspring_per_process)]
+
+        aggr = es.aggregate_results(results)
         for _ in range(num_processes):
-            model_queue.put(model)
-        t4 = time()
+            model_queue.put(aggr)
+
         if save_model:
             save_model(model, model_save_path)
-        t5 = time()
-        print('epoch: {:4}; time: {:5.0f}; best score: {:1.5f}; accuracy: {:1.5f}'.format(
-            epoch, time() - start_time, results[0][0][0], results[0][0][1]))
-        # print('[main] get results   {:2.6f}'.format(t1 - t0))
-        # print('[main] sort          {:2.6f}'.format(t2 - t1))
-        # print('[main] getnextmodel  {:2.6f}'.format(t3 - t2))
-        # print('[main] put to Q      {:2.6f}'.format(t4 - t3))
-        # print('[main] get and save  {:2.6f}'.format(t5 - t4))
+        print('epoch: {:4}; time: {:5.0f}; best score: {:1.5f}'.format(
+            epoch, time() - start_time, results[0][0]))
 
     # We are done.
     for p in processes:
--- regression.py
+++ regression.py
@@ -1,24 +1,97 @@
-import base
 import random
+import math
 
 import numpy as np
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+import base
+from base import ES
+
 
 LR = 0.001
 SIGMA = 0.002
 POLY_DEGREE = 6
 
+
+class Regression(ES):
+
+    def __init__(self, model):
+        super(Regression, self).__init__(model)
+
+    def perturb(self, model, seed):
+        np.random.seed(seed)
+        for w in model.parameters():
+            rands = np.random.normal(0, SIGMA, w.size())
+            noise = torch.from_numpy(rands).float()
+            w.data += noise
+        return model
+
+    def evaluate(self, model):
+        def make_features(x):
+            """Builds features i.e. a matrix with columns [x, x^2, x^3, x^4]."""
+            x = x.unsqueeze(1)
+            return torch.cat([x ** i for i in range(1, POLY_DEGREE+1)], 1)
+
+        def get_batch(batch_size=32):
+            def f(x):
+                """Approximated function."""
+                return x.mm(W_target) + b_target[0]
+
+            """Builds a batch i.e. (x, f(x)) pair."""
+            x = make_features(random)
+            y = f(x)
+            return Variable(x), Variable(y)
+
+        data, real = get_batch(batch_size)
+        output = model(data)
+        err = float(F.smooth_l1_loss(output, real).data[0])
+        return err
+
+    def post_perturb(self, model):
+        '''Probably insert quantization here!'''
+        return model
+
+    def aggregate_results(self, results):
+        '''
+        results: [(evaluate(model), seed), ...]
+        It is sorted. Whatever is returned here is sent to all children and given to
+        `reconstruct()`
+        '''
+        if results[-1][0] == results[0][0]:
+            return [(0, s) for (n, s) in results]
+        normalize = lambda n: 1 - ((n - results[0][0]) / (results[-1][0] - results[0][0]))
+        _sum = sum(normalize(n) for (n, _) in results)
+        return [(normalize(n) / _sum, s) for (n, s) in results]
+
+    def reconstruct(self, model, results):
+        '''
+        `model` is the current full precision model.
+        `results` is whatever aggregate_results() returned.
+        '''
+        if self.debug:
+            print('==> Learned function: ' + poly_desc(model.weight.data.view(-1), model.bias.data))
+            print('==> Actual function:  ' + poly_desc(W_target.view(-1), b_target))
+            print()
+        for param in model.state_dict().values():
+            N = param.size()
+            update = np.zeros(N)
+            for (frac, seed) in results:
+                np.random.seed(seed)
+                rands = np.random.normal(0, 1, N)
+                update += frac * rands
+            update *= 1 / (len(results) * SIGMA ** 2)
+            param += torch.from_numpy(update * LR).float()
+        return model
+
+
 W_target = torch.randn(POLY_DEGREE, 1) * 5
 b_target = torch.randn(1) * 5
 
-
-def get_model():
-    ''' ax + b '''
-    return torch.nn.Linear(W_target.size(0), 1)
-
+model = Regression(torch.nn.Linear(W_target.size(0), 1))
+batch_size = 32
+random = torch.randn(batch_size)
 
 def poly_desc(W, b):
     """Creates a string description of a polynomial."""
@@ -29,91 +102,15 @@ def poly_desc(W, b):
     return result
 
 
-def noise_model(model, seed):
-    '''
-    Perturb the model with the given `seed`.
-    '''
-    np.random.seed(seed)
-    for w in model.parameters():
-        rands = np.random.normal(0, SIGMA, w.size())
-        noise = torch.from_numpy(rands).float()
-        w.data += noise
-    return model
-
-import math
-
-batch_size = 32
-random = torch.randn(batch_size)
-
-def evaluate(model):
-    def make_features(x):
-        """Builds features i.e. a matrix with columns [x, x^2, x^3, x^4]."""
-        x = x.unsqueeze(1)
-        return torch.cat([x ** i for i in range(1, POLY_DEGREE+1)], 1)
-
-    def get_batch(batch_size=32):
-        def f(x):
-            """Approximated function."""
-            return x.mm(W_target) + b_target[0]
-
-        """Builds a batch i.e. (x, f(x)) pair."""
-        x = make_features(random)
-        y = f(x)
-        return Variable(x), Variable(y)
-
-    data, real = get_batch(batch_size)
-    output = model(data)
-    err = float(F.smooth_l1_loss(output, real).data[0])
-    return ((err) / 1, err / batch_size)
-
-
-def get_next_model_main(results):
-    '''
-    results: [(score, seed), ...]
-    It is sorted. Whatever is returned here is sent to all children and given to
-    `get_next_model_child()`
-    '''
-    if results[-1][0] == results[0][0]:
-        return [(0, s) for (n, s) in results]
-    normalize = lambda n: 1 - ((n - results[0][0]) / (results[-1][0] - results[0][0]))
-    _sum = sum(normalize(n) for (n, _) in results)
-    return [(normalize(n) / _sum, s) for (n, s) in results]
-
-
-def get_next_model_child(model, results):
-    '''
-    `model` is the current full precision model.
-    `results` is whatever get_next_model_main() returned.
-    '''
-    if base.RANK == 1:
-        print('==> Learned function: ' + poly_desc(model.weight.data.view(-1), model.bias.data))
-        print('==> Actual function:  ' + poly_desc(W_target.view(-1), b_target))
-        print()
-    for param in model.state_dict().values():
-        N = param.size()
-        update = np.zeros(N)
-        for (frac, seed) in results:
-            np.random.seed(seed)
-            rands = np.random.normal(0, 1, N)
-            update += frac * rands
-        update *= 1 / (len(results) * SIGMA ** 2)
-        param += torch.from_numpy(update * LR).float()
-    return model
-
 
 def run():
     np = 2
-    args = {
-            'get_model': get_model,
-            'noise_model': noise_model,
-            'evaluate': evaluate,
-            'get_next_model_main': get_next_model_main,
-            'get_next_model_child': get_next_model_child,
-            'num_epochs': 2000,
-            'num_processes': np,
-            'perturbs_per_process': 10000 // np,
-    }
-    base.main_loop(args)
+    base.main_loop(
+            model,
+            num_epochs=2000,
+            offspring_per_process=10000 // np,
+            num_processes=np,
+            seed=0)
 
 if __name__ == '__main__':
     run()
